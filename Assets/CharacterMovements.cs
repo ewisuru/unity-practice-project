689c20589b8e42e6eac7cdcf103105e3d5568b4f
Isuru Siriwardana <isuru.siriwardana@zenika.com>
2023-03-22 07:46:54 +0800

    initial commit

---
+++ Assets/CharacterMovements.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovements : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontalInput * 5f, rb.velocity.y, verticalInput * 5f);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
+        }
+    }
+}
